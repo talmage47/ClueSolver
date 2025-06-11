2ce519f62e6c5880eb4254d84cb308614fd873ee
Talmage Gaisford <07.cap-inducer@icloud.com>
2025-06-11 17:03:36 -0600

    no message

---
+++ ClueSolver/Views/ClueSolverApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClueSolverApp.swift
+//  ClueSolver
+//
+//  Created by Talmage Gaisford on 6/11/25.
+//
+
+import SwiftUI
+
+@main
+struct ClueSolverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
