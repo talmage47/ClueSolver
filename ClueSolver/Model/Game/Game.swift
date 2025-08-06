863b2eea6e77eaf9970a3c5c3b41f56c43a440ff
Talmage Gaisford <07.cap-inducer@icloud.com>
2025-08-05 21:24:26 -0600

    HistoryView mostly works now. I need to fix the commit button for editing previous guesses. I added a mockGame static function to Game so I can set up previews more easily

--- ClueSolver/Model/Game/Game.swift
+++ ClueSolver/Model/Game/Game.swift
@@ -30,6 +30,84 @@ class Game: Hashable {
         self.deductionEngine = DeductionEngine(game: self)
     }
 
+    static func mockGame() -> Game {
+        let game = Game()
+        let matthias = Player(playerName: "Matthias")
+        let myla = Player(playerName: "Mylalala")
+        let jon = Player(playerName: "Jonathan")
+        let zach = Player(playerName: "Zachary")
+        let megan = Player(playerName: "Megan")
+        
+        game.players = [
+            matthias, myla, jon, zach, megan
+        ]
+        
+        let scarlet = GameCharacter(characterName: "Miss Scarlet")
+        let mustard = GameCharacter(characterName: "Colonel Mustard")
+        let peacock = GameCharacter(characterName: "Miss Peacock")
+        let green = GameCharacter(characterName: "Mister Green")
+        let white = GameCharacter(characterName: "Mrs White")
+        let plum = GameCharacter(characterName: "Mister Plum")
+    
+        let candlestick = Weapon(weaponName: "Candlestick")
+        let dagger = Weapon(weaponName: "Dagger")
+        let revolver = Weapon(weaponName: "Revolver")
+        let rope = Weapon(weaponName: "Rope")
+        let pipe = Weapon(weaponName: "Pipe")
+        let wrench = Weapon(weaponName: "Wrench")
+    
+        let kitchen = Room(roomName: "Kitchen")
+        let bathroom = Room(roomName: "Bathroom")
+        let closet = Room(roomName: "Closet")
+        let ballroom = Room(roomName: "Ballroom")
+        let mancave = Room(roomName: "Mancave")
+        let library = Room(roomName: "Library")
+        let dining = Room(roomName: "Dining Room")
+        let bedroom = Room(roomName: "Bedroom")
+        let conservatory = Room(roomName: "Conservatory")
+        
+        
+        game.characters = [
+            scarlet, mustard, peacock, green, white, plum
+        ]
+        game.weapons = [
+            candlestick, dagger, revolver, rope, pipe, wrench
+        ]
+        game.rooms = [
+            kitchen, bathroom, closet, ballroom, mancave, library ,dining, bedroom, conservatory
+        ]
+        
+        let guess1 = Guess(userGuess: true)
+        guess1.guesser = matthias
+        guess1.character = green
+        guess1.weapon = revolver
+        guess1.room = closet
+        guess1.passers = [myla]
+        guess1.disprover = megan
+        
+        let guess2 = Guess(userGuess: false)
+        guess2.guesser = myla
+        guess2.character = mustard
+        guess2.weapon = rope
+        guess2.room = ballroom
+        guess2.passers = [jon]
+        // no disprover
+        
+        let guess3 = Guess(userGuess: false)
+        guess3.guesser = zach
+        guess3.character = scarlet
+        guess3.weapon = dagger
+        guess3.room = ballroom
+        guess3.passers = [jon, matthias, megan]
+        guess3.disprover = myla
+        
+        game.guessRecord = [guess1, guess2, guess3]
+        
+        return game
+    }
+    
+
+    
     static func == (lhs: Game, rhs: Game) -> Bool {
         lhs.id == rhs.id
     }
--- ClueSolver/Views/History Views/HistoryView.swift
+++ ClueSolver/Views/History Views/HistoryView.swift
@@ -36,40 +36,9 @@ struct HistoryView: View {
 }
 
 #Preview {
-    // Create mock players
-    let alice = Player(playerName: "Alice")
-    let bob = Player(playerName: "Bob")
-    let carol = Player(playerName: "Carol")
 
     // Create a mock game
-    let game = Game()
-
-    // Create mock guesses
-    let guess1 = Guess(userGuess: true)
-    guess1.guesser = alice
-    guess1.character = GameCharacter(characterName: "Green")
-    guess1.weapon = Weapon(weaponName: "Gun")
-    guess1.room = Room(roomName: "Closet")
-    guess1.passers = [bob]
-    guess1.disprover = carol
-
-    let guess2 = Guess(userGuess: false)
-    guess2.guesser = bob
-    guess2.character = GameCharacter(characterName: "Colonel Mustard")
-    guess2.weapon = Weapon(weaponName: "Rope")
-    guess2.room = Room(roomName: "Ballroom")
-    guess2.passers = [alice]
-    // no disprover
-    
-    let guess3 = Guess(userGuess: false)
-    guess3.guesser = bob
-    guess3.character = GameCharacter(characterName: "Miss Scarlet")
-    guess3.weapon = Weapon(weaponName: "Knife")
-    guess3.room = Room(roomName: "Ballroom")
-    guess3.passers = [alice]
-    guess3.disprover = carol
-
-    game.guessRecord = [guess1, guess2, guess3]
+    let game = Game.mockGame()
 
     return HistoryView(game: game)
 }
