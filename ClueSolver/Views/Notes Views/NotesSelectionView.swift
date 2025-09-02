00fa8b92d984907f2406148b2e90da952b3f7ef3
Talmage Gaisford <07.cap-inducer@icloud.com>
2025-09-02 16:36:51 -0600

    backup before switching SelectableObject from a protocol to classes using inheritance

---
+++ ClueSolver/Views/Notes Views/NotesSelectionView.swift
@@ -0,0 +1,59 @@
+//
+//  NotesSelectionView.swift
+//  ClueSolver
+//
+//  Created by Talmage Gaisford on 9/2/25.
+//
+
+import SwiftUI
+
+struct NotesSelectionView: View {
+    @Bindable var game: Game
+    @Bindable var selectableObjectArray: [any SelectableObject]
+    @State private var selectedObject: (any SelectableObject)? = nil
+    @State private var showSheet: Bool = false
+    
+    var body: some View {
+        ZStack {
+            VStack(alignment: .leading, spacing: 8) {
+                Text("Players")
+                FlowLayout {
+                    ForEach(game.players, id: \.id) { item in
+                        Text(item.displayName)
+                            .padding(10)
+                            .background(Color("UnselectedButton"))
+                            .foregroundColor(.black)
+                            .cornerRadius(8)
+                            .onTapGesture {
+                                selectedObject = item
+                                showSheet = true
+                            }
+                    }
+                }
+                .sheet(isPresented: $showSheet) {
+                    ZStack {
+                        Color("SheetBackground").ignoresSafeArea()
+                        VStack {
+                            RoundedRectangle(cornerRadius: 3)
+                                .fill(Color("UnselectedButton"))
+                                .frame(width: 40, height: 6)
+                                .padding(.top, 8)
+                            PlayerNotesView()
+                        }
+                    }
+                }
+            }
+            .padding()
+            .background(
+                RoundedRectangle(cornerRadius: 12, style: .continuous)
+                    .fill(Color("Foreground"))
+            )
+            .padding(.horizontal)
+            .padding(.vertical, 4)
+        }
+    }
+}
+
+#Preview {
+    NotesSelectionView()
+}
--- ClueSolver/Views/Notes Views/NotesView.swift
+++ ClueSolver/Views/Notes Views/NotesView.swift
@@ -9,46 +9,13 @@ import SwiftUI
 
 struct NotesView: View {
     @Bindable var game: Game
-    @State private var selectedObject: (any SelectableObject)? = nil
-    @State private var showSheet: Bool = false
     
     var body: some View {
         ZStack {
             VStack(alignment: .leading, spacing: 8) {
-                Text("Players")
-                FlowLayout {
-                    ForEach(game.players, id: \.id) { item in
-                        Text(item.displayName)
-                            .padding(10)
-                            .background(Color("UnselectedButton"))
-                            .foregroundColor(.black)
-                            .cornerRadius(8)
-                            .onTapGesture {
-                                selectedObject = item
-                                showSheet = true
-                            }
-                    }
-                }
-                .sheet(isPresented: $showSheet) {
-                    ZStack {
-                        Color("SheetBackground").ignoresSafeArea()
-                        VStack {
-                            RoundedRectangle(cornerRadius: 3)
-                                .fill(Color("UnselectedButton"))
-                                .frame(width: 40, height: 6)
-                                .padding(.top, 8)
-                            PlayerNotesView()
-                        }
-                    }
-                }
+                NotesSelectionView(game: game)
+    
             }
-            .padding()
-            .background(
-                RoundedRectangle(cornerRadius: 12, style: .continuous)
-                    .fill(Color("Foreground"))
-            )
-            .padding(.horizontal)
-            .padding(.vertical, 4)
         }
     }
 }
